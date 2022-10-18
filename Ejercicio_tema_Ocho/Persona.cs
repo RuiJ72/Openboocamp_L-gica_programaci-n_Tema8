c9031215bf64c4921b66495a4ab39a15f633baa2
RuiJ72 <rlagos900@gmail.com>
2022-10-18 20:19:10 -0300

    first commit

---
+++ Ejercicio_tema_Ocho/Persona.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_tema_Ocho
+{
+    class Persona
+    {
+        private string edade;
+        private string nombre;
+        private string telefono;
+
+        public string Edade
+        {
+            get
+            {
+                return edade;
+            }
+            set
+            {
+                edade = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public string Telefono
+        {
+            get
+            {
+                return telefono;
+            }
+            set
+            {
+                telefono = value;
+            }
+        }
+
+
+        public Persona()
+        {
+            edade = string.Empty;
+            nombre = string.Empty;
+            telefono = string.Empty;
+
+        }
+    }
+}
+
---
+++ Ejercicio_tema_Ocho/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ejercicio_tema_Ocho
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            persona.Edade = "50";
+            persona.Nombre = "Rui Lagos";
+            persona.Telefono = "121365888";
+
+
+            Console.WriteLine("La edad es: " + persona.Edade);
+            Console.WriteLine("Nombre: " + persona.Nombre);
+            Console.WriteLine("Telefono: " + persona.Telefono);
+
+        }
+    }
+}
+
+   
